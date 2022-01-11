11bdfc8f4f01edcced08c8b89f2d67a71b15fa67
Batmanlai Tsogt <60453989+btsogt21@users.noreply.github.com>
2022-01-10 20:39:50 -0600

    Add files via upload

--- Python Projects/Project 2 - Scraping from StackExchange with Scrapy and MongoDB/stack/pipelines.py
+++ Python Projects/Project 2 - Scraping from StackExchange with Scrapy and MongoDB/stack/pipelines.py
@@ -19,6 +19,32 @@ import logging
 
 settings = get_project_settings()
 
+# Switch which of the following blocks you are using based on which spider you are crawling with.
+
+
+# stack_spider
+# class MongoDBPipeline(object):
+
+    # def __init__(self):
+        # connection = pymongo.MongoClient(
+            # settings.get('MONGODB_SERVER'),
+            # settings.get('MONGODB_PORT')
+        # )
+        # db = connection[settings.get('MONGODB_DB')]
+        # self.collection = db[settings['MONGODB_COLLECTION']]
+
+    # def process_item(self, item, spider):
+        # valid = True
+        # for data in item:
+            # if not data:
+                # valid = False
+                # raise DropItem("Missing {0}!".format(data))
+        # if valid:
+            # self.collection.insert(dict(item))
+            # logging.debug("Question added to MongoDB database!")
+        # return item
+
+# stack_crawler
 class MongoDBPipeline(object):
 
     def __init__(self):
@@ -30,12 +56,12 @@ class MongoDBPipeline(object):
         self.collection = db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
-        valid = True
         for data in item:
             if not data:
-                valid = False
-                raise DropItem("Missing {0}!".format(data))
-        if valid:
-            self.collection.insert(dict(item))
-            logging.debug("Question added to MongoDB database!")
+                raise DropItem("Missing data!")
+        self.collection.update({'url': item['url']}, dict(item), upsert=True)
+        logging.debug("Question added to MongoDB database!")
         return item
+        
+        
+        
